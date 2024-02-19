ac4b9857dcd4c1967b718ca3028d211638b05f4c
Lettie <lettiezulu764@gmail.com>
2024-02-19 03:21:49 +0200

    Lesson3

--- lessons/Lesson3/src/Lesson3/TestImmutability.java
+++ lessons/Lesson3/src/Lesson3/TestImmutability.java
@@ -1,13 +1,13 @@
-package Lesson3;
-
-public class TestImmutability {
-    public static void main(String[] args) {
-        Employee emp = new Employee(121, "Ron", "ab123", 5000);
-        System.out.println(emp.getName());
-        System.out.println(emp.getSalary());
-        emp.changeName("Lettie Zulu");
-        System.out.println(emp.getName());
-        emp.raiseSalary(2000.00);
-        System.out.println(emp.getSalary());
-    }
-}
+//package Lesson3;
+//
+//public class TestImmutability {
+//    public static void main(String[] args) {
+//        Employee emp = new Employee(121, "Ron", "ab123", 5000);
+//        System.out.println(emp.getName());
+//        System.out.println(emp.getSalary());
+//        emp.hangeName("Lettie Zulu");
+//        System.out.println(emp.getName());
+//        emp.raiseSalary(2000.00);
+//        System.out.println(emp.getSalary());
+//    }
+//}
