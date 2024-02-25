1b0643f030baaabafa309496443588fcb2e807ca
Lettie <lettiezulu764@gmail.com>
2024-02-26 00:11:52 +0200

    Practise10_1

---
+++ lessons/Practice10_1/src/lambda/Main.java
@@ -0,0 +1,12 @@
+package lambda;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MapTest.main(args);
+        PeekTest.main(args);
+
+    }
+}
---
+++ lessons/Practice10_1/src/lambda/State.java
@@ -0,0 +1,19 @@
+package lambda;
+
+public enum State {
+    CA("CA"),
+    CO("CO"),
+    MA("MA");
+
+    private final String str;
+
+    State(String stateStr){
+        this.str = stateStr;
+    }
+
+    public String getStr(){
+        return str;
+    }
+
+}
+
